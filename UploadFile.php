3d293acaca80133480f84e6c1ddf045deae41df6
Ricardo.lagos <ricardolagoshn>
2021-07-13 20:42:57 -0600

    Cambio al Upload

--- UploadFile.php
+++ UploadFile.php
@@ -7,19 +7,26 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once 'Database.php';
-include_once 'Empleados.php';
+include_once 'Fotografias.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-$item = new Empleados($db);
+$item = new Fotografias($db);
 
 
 $image = $_POST["IMAGEN"];
 $decodeImage = base64_decode($image);
 file_put_contents("Image.jpg", $decodeImage);
 
-echo json_encode(
-    array("message" => "Imagen subida"));
+$item->imagen = $decodeImage;
+
+    if($item->StoreImage()){  
+        echo json_encode(
+        array("message" => "Imagen subida"));
+    } else{
+        echo json_encode(
+            array("message" => "Imagen no creada"));
+    }
 
 ?>
